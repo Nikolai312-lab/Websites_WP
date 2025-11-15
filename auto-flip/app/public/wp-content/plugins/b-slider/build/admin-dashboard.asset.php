2a92dc602f89a7589881dbe6ccf151c011994249
m£th <nikint478@gmail.com>
2025-11-15 16:53:12 +0200

    Made changes on AutoFlip

---
+++ auto-flip/app/public/wp-content/plugins/b-slider/build/admin-dashboard.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '44e507c87bb71716b6a8');
---
+++ auto-flip/app/public/wp-content/plugins/b-slider/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => 'fa1f369af87a963edaf3');
